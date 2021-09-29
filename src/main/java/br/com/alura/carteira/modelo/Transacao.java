250c8ae05449c70e6ff629ce257d0d246f66c3aa
edmilsonvaldeciopinton <edmilson.valdecio.pinton@gmail.com>
2021-09-29 20:24:14 -0300

    mapeamento das entidades Transacao e Usuario

--- src/main/java/br/com/alura/carteira/modelo/Transacao.java
+++ src/main/java/br/com/alura/carteira/modelo/Transacao.java
@@ -3,24 +3,43 @@ package br.com.alura.carteira.modelo;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-//import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-
 @Getter
 @Setter
 @ToString(exclude = { "data", "quantidade", "tipo" })
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "transacoes")
 public class Transacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String ticker;
 	private LocalDate data;
 	private BigDecimal preco;
-	private int quantidade;
+	private Integer quantidade;
+
+	@Enumerated(EnumType.STRING)
 	private TipoTransacao tipo;
+
+	@ManyToOne
+	// @JoinColumn(name = "id_usuario")
+	private Usuario usuario;
 
 }
