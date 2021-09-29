27e29dbee74749785aafc4c8a3bf5673b09e3289
edmilsonvaldeciopinton <edmilson.valdecio.pinton@gmail.com>
2021-09-29 17:35:25 -0300

    configurações da JPA

---
+++ carteira-api/src/main/java/br/com/alura/carteira/modelo/Transacao.java
@@ -0,0 +1,26 @@
+package br.com.alura.carteira.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+
+@Getter
+@Setter
+@ToString(exclude = { "data", "quantidade", "tipo" })
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transacao {
+	private String ticker;
+	private LocalDate data;
+	private BigDecimal preco;
+	private int quantidade;
+	private TipoTransacao tipo;
+
+}
