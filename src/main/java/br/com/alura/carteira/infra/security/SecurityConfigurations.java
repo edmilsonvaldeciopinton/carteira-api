517f4d6f8da9e073a76a9314c1368edc2d80b6f4
edmilsonvaldeciopinton <edmilson.valdecio.pinton@gmail.com>
2021-10-25 17:20:35 -0300

    Controle com Acesso com Spring Security

---
+++ src/main/java/br/com/alura/carteira/infra/security/SecurityConfigurations.java
@@ -0,0 +1,35 @@
+package br.com.alura.carteira.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AutenticaoService autenticaoService;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.userDetailsService(autenticaoService).passwordEncoder(bCryptPasswordEncoder);
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().csrf().disable();
+	}
+
+//	public static void main(String[] args) {
+//		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+//	}
+
+}
