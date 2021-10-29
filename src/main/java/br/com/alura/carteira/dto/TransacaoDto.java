6955edd38ad13c54389c393854a25fe326faad62
edmilsonvaldeciopinton <edmilson.valdecio.pinton@gmail.com>
2021-10-29 19:57:48 -0300

    Correção nos Testes que Haviam Bugado com a Parte de Segurança

--- src/main/java/br/com/alura/carteira/dto/TransacaoDto.java
+++ src/main/java/br/com/alura/carteira/dto/TransacaoDto.java
@@ -3,14 +3,18 @@ package br.com.alura.carteira.dto;
 import java.math.BigDecimal;
 
 import br.com.alura.carteira.modelo.TipoTransacao;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransacaoDto {
 
-    private Long id;
+	private Long id;
 	private String ticker;
 	private BigDecimal preco;
 	private Integer quantidade;
--- src/test/java/br/com/alura/carteira/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/alura/carteira/controller/UsuarioControllerTest.java
@@ -5,15 +5,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import javax.transaction.Transactional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.alura.carteira.infra.security.TokenService;
+import br.com.alura.carteira.modelo.Perfil;
+import br.com.alura.carteira.modelo.Usuario;
+import br.com.alura.carteira.repository.PerfilRepository;
+import br.com.alura.carteira.repository.UsuarioRepository;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -25,23 +34,43 @@ class UsuarioControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
+	@Autowired
+	private TokenService tokenService;
+
+	@Autowired
+	private PerfilRepository perfilRepository;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	private String token;
+
+	@BeforeEach
+	public void gerarToken() {
+		Usuario logado = new Usuario("Rodrigo", "rodrigo", "123456");
+		Perfil admin = perfilRepository.findById(1l).get();
+		logado.adicionarPerfil(admin);
+		usuarioRepository.save(logado);
+		Authentication authentication = new UsernamePasswordAuthenticationToken(logado, logado.getLogin());
+		this.token = tokenService.gerarToken(authentication);
+	}
+
 	@Test
 	void naoDeveriaCadastrarUsuarioComDadosIncompletos() throws Exception {
 		String json = "{}";
 
-		mvc.perform(post("/usuarios").contentType(MediaType.APPLICATION_JSON).content(json))
-				.andExpect(status().isBadRequest());
+		mvc.perform(post("/usuarios").contentType(MediaType.APPLICATION_JSON).content(json).header("Authorization",
+				"Bearer " + token)).andExpect(status().isBadRequest());
 
 	}
 
 	@Test
 	void deveriaCadastrarUsuarioComDadosCompletos() throws Exception {
-		String json = "{\"nome\":\"fulano\",\"login\":\"fulano@email.com\"}";
+		String json = "{\"nome\":\"fulano\",\"login\":\"fulano@email.com\", \"perfilId\":1}";
 
-		mvc.perform(post("/usuarios").contentType(MediaType.APPLICATION_JSON).content(json))
-				.andExpect(status().isCreated());
-		// .andExpect(header().exists("Location"))
-		// .andExpect(content().json(json));
-
+		mvc.perform(post("/usuarios").contentType(MediaType.APPLICATION_JSON).content(json).header("Authorization",
+				"Bearer " + token)).andExpect(status().isCreated());
+		// .andExpect(header().exist("Location"))
+		// .andExpect(content().json(json))
 	}
 }
